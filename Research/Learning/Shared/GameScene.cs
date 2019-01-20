029f216c9dc1e5d51b2d16cb0dde1bc7691e3282
Raphael Gournay <eoraptorproject@gmail.com>
2019-01-20 22:28:43 +0000

    Experimentation
    I have been experimenting with the Learning file in order to be able to learn to use the SpriteKit library w/ Visual Studio. Looking to move the sprite around the screen and change the sprite's image

--- Research/Learning/Shared/GameScene.cs
+++ Research/Learning/Shared/GameScene.cs
@@ -14,21 +14,28 @@ namespace SpriteKitGame
 {
     public class GameScene : SKScene
     {
+        SKSpriteNode bg = SKSpriteNode.FromImageNamed("person");
+
         protected GameScene(IntPtr handle) : base(handle)
         {
+
             // Note: this .ctor should not contain any initialization logic.
         }
 
         public override void DidMoveToView(SKView view)
         {
             // Setup your scene here
-            var myLabel = new SKLabelNode("Chalkduster")
+            var myLabel = new SKLabelNode("ArialMT")
             {
-                Text = "This sucks",
+                Text = "Can change text and bgcolor",
                 FontSize = 50,
                 Position = new CGPoint(Frame.Width / 2, Frame.Height / 2)
             };
 
+            //BackgroundColor = NSColor.Black;
+            bg.Position = new CGPoint(XScale = Frame.Size.Width / 2, YScale = Frame.Size.Height / 2);
+
+            AddChild(bg);
             AddChild(myLabel);
         }
 
@@ -39,7 +46,7 @@ namespace SpriteKitGame
 			foreach (var touch in touches) {
 				var location = ((UITouch)touch).LocationInNode (this);
 
-				var sprite = new SKSpriteNode ("Spaceship") {
+				var sprite = new SKSpriteNode ("logo") {
 					Position = location,
 					XScale = 0.5f,
 					YScale = 0.5f
@@ -58,18 +65,18 @@ namespace SpriteKitGame
             // Called when a mouse click occurs
 
             var location = theEvent.LocationInNode(this);
-
-            var sprite = SKSpriteNode.FromImageNamed(NSBundle.MainBundle.PathForResource("Spaceship", "png"));
+            SKSpriteNode sprite = SKSpriteNode.FromImageNamed("person");
             //var sprite = SKSpriteNode.FromImageNamed(NSBundle.MainBundle.PathForResource("Spaceship", "png"));
 
             sprite.Position = location;
             sprite.SetScale(0.5f);
 
-            var action = SKAction.RotateByAngle(NMath.PI, 1.0);
+            var action = SKAction.MoveBy(2, 2, 0.1);
 
             sprite.RunAction(SKAction.RepeatActionForever(action));
-
+            //sprite.RunAction(SKAction.RepeatActionForever(action));
             AddChild(sprite);
+
         }
 #endif
 
